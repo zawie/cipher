95de9c57d92c5245e166171b5d9e8527313eb9e9
Adam Zawierucha <zawiedev@gmail.com>
2025-03-08 23:20:55 -0800

    🍦 Serve chat page

--- main.go
+++ main.go
@@ -1,25 +1,21 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
 )
-
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, HTTPS!")
-}
 
 // TODO: Read values from environment variables
 var certPath = "cert.pem"
 var keyPath = "key.pem"
 
 func main() {
-	http.HandleFunc("/", handler)
+	// Serve static files inside each page directory
+	http.Handle("/chat/", http.StripPrefix("/chat/", http.FileServer(http.Dir("frontend/chat"))))
 
-	// Use self-signed certificate (or a real one if available)
-	log.Println("Starting HTTPS server on https://localhost:8443")
-	err := http.ListenAndServeTLS(":8443", certPath, keyPath, nil)
+	log.Println("Serving on https://localhost:8443")
+
+	err := http.ListenAndServeTLS(":8443", "cert.pem", "key.pem", nil)
 	if err != nil {
 		log.Fatal("ListenAndServeTLS: ", err)
 	}
